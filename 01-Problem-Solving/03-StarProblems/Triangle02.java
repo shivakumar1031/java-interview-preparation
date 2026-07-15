0b5ad8983b9f34405c05cf159d25537d4e5e72a6
shivakumar1031 <shivakumarchamanthula@gmail.com>
2026-07-15 07:58:04 +0530

    star problems first commit

---
+++ 01-Problem-Solving/03-StarProblems/Triangle02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Triangle02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = sc.nextInt();
+        for(int i=0;i<number;i++){
+            for(int j=i;j<number;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
