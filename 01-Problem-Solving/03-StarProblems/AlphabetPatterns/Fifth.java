c0f32df4eece997389846c9bf6c0257611ca355d
shivakumar1031 <shivakumarchamanthula@gmail.com>
2026-07-19 10:05:54 +0530

    modification commit

--- 01-Problem-Solving/03-StarProblems/AlphabetPatterns/Fifth.java
+++ 01-Problem-Solving/03-StarProblems/AlphabetPatterns/Fifth.java
@@ -8,7 +8,7 @@ A B C D E
 
 import java.util.Scanner;
 
-public class Four {
+public class Fifth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of Rows :");
